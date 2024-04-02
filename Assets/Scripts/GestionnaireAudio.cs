70a28b185323f8cabd7efeeba463db3eb38469ff
AlexStLo <alex.st-louis@hotmail.com>
2024-04-02 16:54:22 -0400

    les beau ptis sliders (pas les burgers miam j'ai un peut faim maintenant)

---
+++ Assets/Scripts/GestionnaireAudio.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+
+public class GestionnaireAudio : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer audioMixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     //  audioMixer.SetFloat("VolumeMusique",-30f); 
+    }
+    public void AjusteVolumeMusique(float volume){
+        audioMixer.SetFloat("VolumeMusique", ConvertToLogarithmique(volume) ); 
+    }
+       public void AjusteVolumeFX(float volume){
+        audioMixer.SetFloat("VolumeFX", ConvertToLogarithmique(volume) ); 
+    }
+
+    private float ConvertToLogarithmique(float volume){
+        return Mathf.Log10(volume) * 20;
+    }
+}
